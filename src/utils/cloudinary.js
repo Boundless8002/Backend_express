5d57ef8cd04086dc4082589cd6ba598dbac6a278
Amit Singh <amit_rajawat8002@yahoo.in>
2026-07-15 19:59:49 +0530

    updated

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,39 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config({
+    path:"./.env"
+})
+
+ cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET 
+    });
+
+    const uploadOnCloudinary=async (localFilePath)=>{
+try {
+    if(!localFilePath) return null
+    // upload  file on cloudinary
+    const response=await cloudinary.uploader.upload(localFilePath,{
+       resource_type:"auto"
+    })
+    // if file has been uploaded successfully
+    console.log("File is upload on cloudinary",response.url)
+    fs.unlinkSync(localFilePath)
+    return response;
+} catch (error) {
+    console.error("Cloudinary Upload Error:", error);
+
+    if (fs.existsSync(localFilePath)) {
+        fs.unlinkSync(localFilePath);
+    }
+
+    return null;
+}
+    }
+
+    export {uploadOnCloudinary}
+
+
+    
