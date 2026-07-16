cce338bfbdff50aadbbcfaa912364f96325e4a54
Amit Singh <amit_rajawat8002@yahoo.in>
2026-07-16 19:44:48 +0530

    Add subscriber model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -138,7 +138,7 @@ await User.findByIdAndUpdate(req.user._id,
 const refreshAccessToken=asyncHandler(async(req,res)=>{
     const incomingRefreshToken=req.cookies.refreshToken || req.body.refreshToken
 
-    if(incomingRefreshToken){
+    if(!incomingRefreshToken){
         throw new ApiError(401,"unauthorized request")
     }
 
